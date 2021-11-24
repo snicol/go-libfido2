14b30129373c46407c7155205667239dbeece3ae
Scott Nicol <s.cott@me.com>
2021-11-24 16:58:24 +0000

    target ARM cflags and ldflags, add darwin ARM libcbor

--- fido2_static.go
+++ fido2_static.go
@@ -1,7 +1,9 @@
 package libfido2
 
 /*
-#cgo darwin LDFLAGS: -framework CoreFoundation -framework IOKit /opt/homebrew/lib/libfido2.a /opt/homebrew/opt/openssl@1.1/lib/libcrypto.a /usr/local/lib/libfido2.a /usr/local/opt/openssl@1.1/lib/libcrypto.a ${SRCDIR}/darwin/lib/libcbor.a
-#cgo darwin CFLAGS: -I/opt/homebrew/opt/libfido2/include -I/opt/homebrew/opt/openssl@1.1/include -I/usr/local/opt/libfido2/include -I/usr/local/opt/openssl@1.1/include
+#cgo darwin,!arm64 LDFLAGS: -framework CoreFoundation -framework IOKit /usr/local/lib/libfido2.a /usr/local/opt/openssl@1.1/lib/libcrypto.a ${SRCDIR}/darwin/lib/libcbor.x86.a
+#cgo darwin,arm64 LDFLAGS: -framework CoreFoundation -framework IOKit /opt/homebrew/lib/libfido2.a /opt/homebrew/opt/openssl@1.1/lib/libcrypto.a ${SRCDIR}/darwin/lib/libcbor.arm.a
+#cgo darwin,!arm64 CFLAGS: -I/usr/local/opt/libfido2/include -I/usr/local/opt/openssl@1.1/include
+#cgo darwin,arm64 CFLAGS: -I/opt/homebrew/opt/libfido2/include -I/opt/homebrew/opt/openssl@1.1/include
 */
 import "C"
